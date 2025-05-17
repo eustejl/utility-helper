3b6f151bc528f2c9478159af3686b75d8ecaa22f
John Lawrence Euste <83872920+eustejl@users.noreply.github.com>
2025-05-17 15:46:58 +0200

    Add files via upload

---
+++ combine_pdf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#__author__ = 'John Lawrence Euste'
+
+# define the individual pdf files
+# there could be more than 2 files
+pdf1=pdf1.pdf # !
+pdf2=pdf2.pdf # !
+
+
+# define the output PDF file
+output_pdf=FilenameOfPDF.pdf  # !
+
+pdfunite $pdf1 $pdf2 $output_pdf # !
+
+exit
+
+# =============================================================================
+# This combines two or more pdf files (here as given in the variables pdf1 and
+#  pdf2) into one pdf file named as given in output_pdf. 
+# =============================================================================
